56159762347c8cc222239f84ec6204c0f9d579bd
Tetali sai charan <121687670+Saicharantetali@users.noreply.github.com>
2023-10-04 06:58:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+async function fetchIp(){
+  try{
+      let response=await fetch('https://api.ipify.org?format=json');
+      let result=await response.json();
+      localStorage.setItem('ip',JSON.stringify(result.ip));
+      displayIp(result.ip);
+  }
+  catch{
+      alert('Error in Fetching IP Address');
+  }
+}
+
+fetchIp();
+
+
+function displayIp(ip){
+  const ipAddDiv=document.getElementsByClassName('ipAdd')[0];
+  ipAddDiv.innerText=ip;
+}
+
+let submitBtn=document.getElementById('submitBtn');
+submitBtn.addEventListener('click',()=>{
+  window.location.href='./details.html'
+})
